a78a6aaa750649077d23d4eb060672cdf7e936c7
kharelabhishek <kharela86@gmail.com>
2024-03-21 16:04:14 +0545

    feat- role based route protected

--- quizclient/middleware.ts
+++ quizclient/middleware.ts
@@ -1,6 +1,69 @@
-// Protecting routes with next-auth
-// https://next-auth.js.org/configuration/nextjs#middleware
-// https://nextjs.org/docs/app/building-your-application/routing/middleware
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export { default } from "next-auth/middleware";
-export const config = { matcher: ["/dashboard/:path*"] };
+export async function middleware(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+  const userToken = token?.user as { type: string };
+
+  const path = request.nextUrl.pathname;
+
+  const isPublicPath = path === "/signin" || path === "/signup" || path === "/";
+
+  if (path === "/" && !token) {
+    return NextResponse.redirect(new URL("/signin", request.nextUrl));
+  }
+
+  if (isPublicPath && token) {
+    if (userToken.type === "admin") {
+      return NextResponse.redirect(new URL("/admin/dashboard", request.nextUrl));
+    } else if (userToken.type === "user") {
+      return NextResponse.redirect(new URL("/dashboard", request.nextUrl));
+    }
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/signin", request.nextUrl));
+  }
+  //   if (isPublicPath && token) {
+  //     return NextResponse.redirect(new URL("/dashboard", request.nextUrl));
+  //   }
+
+  //   if (!isPublicPath && !token) {
+  //     return NextResponse.redirect(new URL("/signin", request.nextUrl));
+  //   }    `
+
+  // route check for admin user
+
+
+
+  // if (
+  //   userToken?.type === "admin" &&
+  //   !!request.nextUrl.pathname.startsWith("/dashboard")
+  // ) {
+  //   return NextResponse.rewrite(new URL("/denied", request.url));
+  // } else if (
+  //   userToken?.type === "user" &&
+  //   !!request.nextUrl.pathname.startsWith("/admin")
+  // ) {
+  //   return NextResponse.rewrite(new URL("/denied", request.url));
+  // }
+
+  if (
+    userToken?.type === "admin" &&
+    !!request.nextUrl.pathname.startsWith("/dashboard")
+  ) {
+    return NextResponse.rewrite(new URL("/denied", request.url));
+  }else if (userToken?.type === "user" &&
+  !!request.nextUrl.pathname.startsWith("/admin")){
+    return NextResponse.rewrite(new URL("/denied", request.url));
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/", "/dashboard/:path*", "/admin/:path*", "/signin", "/signup"],
+};
